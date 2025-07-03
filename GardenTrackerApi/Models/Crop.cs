310713d139e55ba18b5bdf58b857873b880a0f12
Jeremiah Pritchard <jeremiah3643@gmail.com>
2025-07-03 07:23:48 -0500

    forgot to add files

---
+++ GardenTrackerApi/Models/Crop.cs
@@ -0,0 +1,11 @@
+namespace GardenTrackerApi.Models
+{
+    public class Crop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime PlantedDate { get; set; }
+        public string Notes { get; set; }
+        public List<Harvest> Harvests { get; set; }
+    }
+}
